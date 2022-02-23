6e79ee8a2d31415965ed56cf9c12edc282af482b
Shah rahman <toshah.rahman@gmail.com>
2022-02-23 17:22:24 +0200

    step definitions -Then

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -9,3 +9,7 @@ When('I fill the login form with valid credentials', async function() {
     await page.fill('#password','secret_sauce')
     await page.click("#login-button")
 })
+
+Then('I should see the home page', async function() {
+    await page.waitForSelector('.title')
+})
