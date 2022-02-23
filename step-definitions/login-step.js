454c49f6d8328196296725dd5279437dfd1777dd
Shah rahman <toshah.rahman@gmail.com>
2022-02-23 17:14:16 +0200

    step definitions -When

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -3,3 +3,9 @@ const {Given, When, Then, definestep} = require('@cucumber/cucumber')
 Given('I visit a login page', async function() {
     await page.goto('https://www.saucedemo.com/')
 })
+
+When('I fill the login form with valid credentials', async function() {
+    await page.fill('#user-name','standard_user')
+    await page.fill('#password','secret_sauce')
+    await page.click("#login-button")
+})
