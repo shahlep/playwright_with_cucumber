e7242a0c3cb9bc18875005bc56806626222a41a1
Shah rahman <toshah.rahman@gmail.com>
2022-02-24 06:37:41 +0200

    reporter setup,update and run test

--- reporter.js
+++ reporter.js
@@ -3,7 +3,10 @@ const reporter = require('cucumber-html-reporter')
 const options = {
     theme: 'bootstrap',
     jsonFile: 'cucumber_report.json',
-    output: 'reports/cucumber_report.html'
+    output: 'reports/cucumber_report.html',
+    reportSuiteAsScenario: true,
+    scenarioTimestamp: true,
+    launchReport: false,
 }
 
 reporter.generate(options)
